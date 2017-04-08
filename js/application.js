0ad9652d7d378c1e7c9b7e9d51ec41336aae2016
allyshsaye <allisonshaye@gmail.com>
2017-04-08 14:34:20 -0700

    Add stylesheet

--- js/application.js
+++ js/application.js
@@ -1,27 +1,30 @@
+function gameHandler(e) {
+  var keyPressed = e.which
+  if (keyPressed === 38) {
+    game.moveUp()
+  } else if (keyPressed === 40) {
+    game.moveDown()
+  } else if (keyPressed === 37) {
+    game.moveLeft()
+  } else if (keyPressed === 39) {
+    game.moveRight()
+  }
+
+  game.spawnBlock()
+
+  if (game.isOver()) {
+    console.log("gameOver") // not working correctly
+  } else {
+    game.displayBoard()
+  }
+}
+
 $(document).ready(function() {
 
   game = new Game()
   game.displayBoard()
 
-  $('body').on('keyup', function(e) {
-      var keyPressed = e.which
-      console.log(keyPressed)
-      if (keyPressed === 38) {
-        console.log('up')
-        game.moveUp()
-      } else if (keyPressed === 40) {
-        console.log('down')
-        game.moveDown()
-      } else if (keyPressed === 37) {
-        console.log("left")
-        game.moveLeft()
-      } else if (keyPressed === 39) {
-        console.log("right")
-        game.moveRight()
-      }
-      game.spawnBlock()
-      game.displayBoard()
-  })
+  $('body').on('keyup', gameHandler)
 
 })
 
